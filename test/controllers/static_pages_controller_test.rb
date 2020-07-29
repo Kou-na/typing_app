73c500bb3735f839174983a35ebdedbc456ef50f
Kou-na <knarumi0_1q@yahoo.co.jp>
2020-07-29 12:25:51 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # titleで同じ文言を繰り返す時用
+  def setup
+    @base_title = "grow typing"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title",
+    "Home
+| #{@base_title}"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title",
+    "Home
+| #{@base_title}"
   end
 
 end
